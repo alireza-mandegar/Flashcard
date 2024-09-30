c4ffecf6ae2e8b66141b06f30c24be4697c482b7
alireza-mandegar <alireza.mani95@gmail.com>
2024-09-30 15:32:09 +0330

    create the functionality of communicate with db

---
+++ db.py
@@ -0,0 +1,37 @@
+# db.py
+from pymongo import MongoClient
+
+class Database:
+    def __init__(self, db_name: str, collection_name: str):
+        self.client = MongoClient('mongodb://localhost:27017/')
+        self.db = self.client[db_name]
+        self.collection = self.db[collection_name]
+
+    def insert_flashcard(self, flashcard: dict):
+        """
+        Inserts a new flashcard into the collection.
+        """
+        return self.collection.insert_one(flashcard)
+
+    def delete_flashcard_by_question(self, question: str):
+        """
+        Deletes a flashcard by its question text.
+        """
+        return self.collection.delete_one({'question': question})
+
+    def get_all_flashcards(self):
+        """
+        Retrieves all flashcards from the collection.
+        """
+        return list(self.collection.find())
+
+    def search_flashcards_by_keyword(self, keyword: str):
+        """
+        Searches flashcards based on a keyword found in the question or answer.
+        """
+        return list(self.collection.find({
+            '$or': [
+                {'question': {'$regex': keyword, '$options': 'i'}},
+                {'answer': {'$regex': keyword, '$options': 'i'}}
+            ]
+        }))
